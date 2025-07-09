101c1ab13c42638b59ff087b7ee782c259f0e9ad
Nishant shaligram <nishant.shaligram@gmail.com>
2025-07-09 13:36:45 +0530

    Add intro section pattern for featured posts with styling and content

---
+++ patterns/intro-section.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Title: Featured Posts
+ * Slug: indie-writer/intro-section
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|iw-large","bottom":"var:preset|spacing|iw-large","left":"var:preset|spacing|iw-small","right":"var:preset|spacing|iw-small"}}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group"
+        style="padding-top:var(--wp--preset--spacing--iw-large);padding-right:var(--wp--preset--spacing--iw-small);padding-bottom:var(--wp--preset--spacing--iw-large);padding-left:var(--wp--preset--spacing--iw-small)">
+        <!-- wp:columns {"verticalAlignment":"center"} -->
+        <div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
+            <div class="wp-block-column is-vertically-aligned-center">
+                <!-- wp:image {"id":1943,"width":"400px","aspectRatio":"3/4","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-default","style":{"color":{"duotone":"var:preset|duotone|grayscale"}}} -->
+                <figure class="wp-block-image size-full is-resized is-style-default"><img
+                        src="http://localhost:10053/wp-content/uploads/2025/07/caucassian-model-portrait.png" alt=""
+                        class="wp-image-1943" style="aspect-ratio:3/4;object-fit:cover;width:400px" /></figure>
+                <!-- /wp:image -->
+            </div>
+            <!-- /wp:column -->
+
+            <!-- wp:column {"verticalAlignment":"center"} -->
+            <div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"fontSize":"iw-xl"} -->
+                <h2 class="wp-block-heading has-iw-xl-font-size">I write for those who wear confidence like couture.
+                </h2>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph {"fontSize":"medium"} -->
+                <p class="has-medium-font-size">From the runways of Milan to the ateliers of Paris, my words decode the
+                    world of luxury with attitude and elegance. Whether you're obsessed with heritage houses or on the
+                    hunt for the next It Brand, this blog is your front-row seat to the style conversation that matters.
+                </p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:column -->
+        </div>
+        <!-- /wp:columns -->
+    </div>
+<!-- /wp:group -->
