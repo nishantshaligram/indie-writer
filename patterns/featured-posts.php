7a08b17bdca32a95b4d5e6f23294e6514d278c34
Nishant shaligram <nishant.shaligram@gmail.com>
2025-07-09 13:36:36 +0530

    Add featured posts and quote patterns; update front page and theme.json for typography and styles

---
+++ patterns/featured-posts.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Title: Featured Posts
+ * Slug: indie-writer/featured-posts
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|iw-extra-large","bottom":"var:preset|spacing|iw-extra-large","left":"var:preset|spacing|iw-small","right":"var:preset|spacing|iw-small"}}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-group"
+        style="padding-top:var(--wp--preset--spacing--iw-extra-large);padding-right:var(--wp--preset--spacing--iw-small);padding-bottom:var(--wp--preset--spacing--iw-extra-large);padding-left:var(--wp--preset--spacing--iw-small)">
+        <!-- wp:columns -->
+        <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","width":"50%"} -->
+            <div class="wp-block-column is-vertically-aligned-top" style="flex-basis:50%">
+                <!-- wp:heading {"fontSize":"iw-xl"} -->
+                <h2 class="wp-block-heading has-iw-xl-font-size">Featured Blog Teasers</h2>
+                <!-- /wp:heading -->
+
+                <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
+                <div class="wp-block-buttons"><!-- wp:button -->
+                    <div class="wp-block-button"><a class="wp-block-button__link wp-element-button">View All Posts</a>
+                    </div>
+                    <!-- /wp:button -->
+                </div>
+                <!-- /wp:buttons -->
+            </div>
+            <!-- /wp:column -->
+
+            <!-- wp:column {"verticalAlignment":"top","width":"50%"} -->
+            <div class="wp-block-column is-vertically-aligned-top" style="flex-basis:50%">
+                <!-- wp:group {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|iw-extra-large"}}},"layout":{"type":"default"}} -->
+                <div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--iw-extra-large)">
+                    <!-- wp:query {"queryId":2,"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"ignore","inherit":false,"taxQuery":null,"parents":[],"format":[]}} -->
+                    <div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"default","columnCount":3}} -->
+                        <!-- wp:group {"tagName":"article","style":{"dimensions":{"minHeight":"25px"}},"layout":{"type":"default"}} -->
+                        <article class="wp-block-group" style="min-height:25px">
+                            <!-- wp:post-title {"level":3,"isLink":true,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|iw-small"}}},"fontSize":"iw-m"} /-->
+
+                            <!-- wp:post-excerpt {"showMoreOnNewLine":false,"excerptLength":50,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|iw-medium"}}},"fontSize":"iw-sm"} /-->
+
+                            <!-- wp:read-more {"content":"Read More","fontSize":"iw-sm"} /-->
+                        </article>
+                        <!-- /wp:group -->
+                        <!-- /wp:post-template -->
+                    </div>
+                    <!-- /wp:query -->
+                </div>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:column -->
+        </div>
+        <!-- /wp:columns -->
+    </div>
+<!-- /wp:group -->
---
+++ patterns/quote.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Quote
+ * Slug: indie-writer/quote
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|iw-extra-large","bottom":"var:preset|spacing|iw-extra-large","left":"var:preset|spacing|iw-small","right":"var:preset|spacing|iw-small"}}},"layout":{"type":"constrained","contentSize":"640px"}} -->
+<div class="wp-block-group"
+    style="padding-top:var(--wp--preset--spacing--iw-extra-large);padding-right:var(--wp--preset--spacing--iw-small);padding-bottom:var(--wp--preset--spacing--iw-extra-large);padding-left:var(--wp--preset--spacing--iw-small)">
+    <!-- wp:quote {"className":"is-style-default"} -->
+    <blockquote class="wp-block-quote is-style-default"><!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+        <p class="has-text-align-center has-medium-font-size">“Luxury isn’t about labels. It’s about presence. And
+            my words dress that presence.”</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"300","textTransform":"uppercase"}},"fontSize":"medium"} -->
+        <p class="has-text-align-center has-medium-font-size"
+            style="font-style:normal;font-weight:300;text-transform:uppercase">- John Doe</p>
+        <!-- /wp:paragraph -->
+    </blockquote>
+    <!-- /wp:quote -->
+</div>
+<!-- /wp:group -->
