ab616d31eedeba08bb382ba327e6d5959abd0d5e
Nishant shaligram <nishant@brightpeeps.com>
2025-07-16 15:08:19 +0530

    make footer columns mobile responsive. Left align Block Quote text. Instagram Gallery Section added.

--- patterns/quote.php
+++ patterns/quote.php
@@ -10,15 +10,16 @@
         style="padding-top:var(--wp--preset--spacing--iw-xl);padding-right:var(--wp--preset--spacing--iw-sm);padding-bottom:var(--wp--preset--spacing--iw-xl);padding-left:var(--wp--preset--spacing--iw-sm)">
         <!-- wp:quote {"className":"is-style-default","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|iw-sm"}}}} -->
         <blockquote class="wp-block-quote is-style-default" style="margin-bottom:var(--wp--preset--spacing--iw-sm)">
-            <!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"italic","fontWeight":"600"}},"fontSize":"iw-l"} -->
-            <p class="has-text-align-center has-iw-l-font-size" style="font-style:italic;font-weight:600">Luxury isn’t about labels. It’s about presence. And my words dress that presence.</p>
+            <!-- wp:paragraph {"align":"left","style":{"typography":{"fontStyle":"italic","fontWeight":"600"}},"fontSize":"iw-l"} -->
+            <p class="has-text-align-left has-iw-l-font-size" style="font-style:italic;font-weight:600">Luxury isn’t
+                about labels. It’s about presence. And my words dress that presence.</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:paragraph {"align":"left","style":{"typography":{"fontStyle":"normal","fontWeight":"300","textTransform":"uppercase"}},"fontSize":"iw-sm"} -->
+            <p class="has-text-align-left has-iw-sm-font-size"
+                style="font-style:normal;font-weight:300;text-transform:uppercase">- John Doe</p>
             <!-- /wp:paragraph -->
         </blockquote>
         <!-- /wp:quote -->
-
-        <!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"300","textTransform":"uppercase"}},"fontSize":"iw-sm"} -->
-        <p class="has-text-align-center has-iw-sm-font-size"
-            style="font-style:normal;font-weight:300;text-transform:uppercase">- John Doe</p>
-        <!-- /wp:paragraph -->
     </div>
 <!-- /wp:group -->
